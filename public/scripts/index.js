1c3e530713719c9f7b7974cdc7edcdb76298e2a7
Alka Sah <alkasah485@gmail.com>
2024-06-13 13:03:44 -0700

    modfication in css

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,3 +4,10 @@ const tipContainer  = document.getElementById('tip-container');
 //console.log(tipContainer);
 const fbBtn = document.getElementById('feedback-btn');
 //console.log(fbBtn);
+
+fbBtn.addEventListener("click" , (e) => {
+    e.preventDefault();
+    //console.log("click");
+    window.location.href = "/feedback";
+
+})
