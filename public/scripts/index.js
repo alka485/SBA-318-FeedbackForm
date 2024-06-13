64a9d86aeb9b3931571b626f9815129391f6a889
Alka Sah <alkasah485@gmail.com>
2024-06-13 12:59:20 -0700

    modfication in index.js

---
+++ public/scripts/index.js
@@ -0,0 +1,6 @@
+const tipForm = document.getElementById('tip-form');
+//console.log(tipForm);
+const tipContainer  = document.getElementById('tip-container');
+//console.log(tipContainer);
+const fbBtn = document.getElementById('feedback-btn');
+//console.log(fbBtn);
