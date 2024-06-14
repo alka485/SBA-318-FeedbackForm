0ea0962f1807a34fd11947d2a03172c4dcead2c4
Alka Sah <alkasah485@gmail.com>
2024-06-14 11:23:57 -0700

    added routes structure

---
+++ routes/tips.js
@@ -0,0 +1,4 @@
+const tips = require('express').Router(); //creating a new router instance and assigning it to the constant tips
+const {v4 : uuid} = require('uuid');
+//const newUuid = uuidv4();
+//console.log(newUuid);
