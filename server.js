a9aa05e43c1c8c4c4d8e0c7606a1580ee4416c8e
Alka Sah <alkasah485@gmail.com>
2024-06-15 23:03:23 -0700

    modification in server.js

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
