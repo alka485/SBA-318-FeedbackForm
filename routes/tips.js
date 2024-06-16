3b45632748b49e0f2114677aa2da822a3b5515a1
Alka Sah <alkasah485@gmail.com>
2024-06-15 23:11:50 -0700

    modification in server.js

--- routes/tips.js
+++ routes/tips.js
@@ -1,2 +1,19 @@
 const tips = require('express').Router(); //creating a new router instance and assigning it to the constant tips
 const {v4 : uuid} = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile
+    
+} = require('../helpers/fsUtil')
+
+// GET Route for retrieving all the tips
+tips.get('/', (req,res) => {
+    readFromFile('../db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific tip
+tips.get('/:tip_id', (req, res) => {
+    const tipId = req.params.tip_id;
+    
+})
